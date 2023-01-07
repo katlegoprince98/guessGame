510c33a71748e456e77108b23d486a6f8d2544fe
Katlego Komane <113278179+CynicsTech@users.noreply.github.com>
2023-01-07 16:14:36 +0200

    555

--- script.js
+++ script.js
@@ -6,7 +6,9 @@ let number = [math.floor(Math.random() * 5)];
 btn.addEventListener('click', function(){
      let input = document.getElementById('userInput').value;
      if(input == number){
-        output.innerHTML = '';
+        output.innerHTML = "You guessed correctly, your guessed number ${number}"
+     }else if(input < number){
+        output.innerHTML = "Too low, guess again"
      }
 
 });
