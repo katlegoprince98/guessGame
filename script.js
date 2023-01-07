ccba47205236a5efc7be8bf4597cbe0224b8a8dd
Katlego Komane <113278179+CynicsTech@users.noreply.github.com>
2023-01-07 16:08:59 +0200

    88

--- script.js
+++ script.js
@@ -2,3 +2,11 @@ let btn = document.getElementById('btn');
 let output = document.getElementById('output');
 
 let number = [math.floor(Math.random() * 5)];
+
+btn.addEventListener('click', function(){
+     let input = document.getElementById('userInput').value;
+     if(input == number){
+        output.innerHTML = '';
+     }
+
+});
