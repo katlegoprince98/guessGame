1c18f2e1b11252bedd1d02edd4ebc193a6d060ca
Katlego Komane <113278179+CynicsTech@users.noreply.github.com>
2023-01-07 16:25:07 +0200

    55

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 let btn = document.getElementById('btn');
 let output = document.getElementById('output');
 
-let number = [math.floor(Math.random() * 100)];
+let number = [Math.floor(Math.random() * 100)];
 
 btn.addEventListener('click', function(){
-     let input = document.getElementById('userInput').Value;
+     let input = document.getElementById('userInput').value;
      if(input == number){
         output.innerHTML = "You guessed correctly, your guessed number ${number}"
      }else if(input < number){
